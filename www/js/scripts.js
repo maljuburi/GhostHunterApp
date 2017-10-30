fe9ed1a1a18f71bbee2ee149a9ab97e1282fe3f6
Muhammad <maljuburi89@gmail.com>
2017-10-30 10:48:30 -0400

    Audio files is added to the javascript but not activated yet

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -2,6 +2,16 @@ document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
 
+
+  // Background Audio
+  // ============
+  // var audio = new Audio("../audio/halloween.mp3");
+  // audio.loop = true;
+  // audio.play();
+  
+
+  // Flash Torch
+  // ============
   $(".flash #on").on('click', function () {
     $("#on").hide();
     $("#off").show();
@@ -14,12 +24,17 @@ function onDeviceReady() {
   });
 
 
+
+  // Camera Plugin
+  // ============
   $("#camera").on('click', takephoto);
 
   function takephoto() {
     navigator.camera.getPicture(onSuccess, onFail, {
       quality: 50,
-      destinationType: Camera.DestinationType.FILE_URI
+      destinationType: Camera.DestinationType.FILE_URI,
+      encodingType: Camera.EncodingType.JPEG,
+      correctOrientation: true
     });
 
     function onSuccess(imageURI) {
